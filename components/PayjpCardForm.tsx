7aa12418f7ccbce7e8dfe40ca82fd3a238fdf90f
Hiroyuki Matsuo <h.matsuo.engineer@gmail.com>
2020-06-02 06:03:01 +0900

    Prettier output with Ant Design

--- components/PayjpCardForm.tsx
+++ components/PayjpCardForm.tsx
@@ -1,4 +1,6 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useCallback } from 'react'
+import { Button, Alert, Card, Space } from 'antd'
+import * as Payjp from 'payjp'
 import { usePayjp } from '@/services/PayjpJs'
 
 const PayjpCardForm: React.FC = () => {
@@ -17,24 +19,57 @@ const PayjpCardForm: React.FC = () => {
     }
   }, [payjp])
 
-  const [token, setToken] = useState('トークンを作成するとここに表示されます。')
+  const [token, setToken] = useState<string | null>(null)
+  const [tokenGenerationError, setTokenGenerationError] = useState<Payjp.ResponseError | Error | null>(null)
+
+  const handleClick = useCallback((event: MouseEvent) => {
+    setToken(null)
+    setTokenGenerationError(null)
+    const cardElement = elements.getElement('cardNumber')
+    payjp.createToken(cardElement)
+      .then((response: any) => {
+        if (response.error) {
+          console.error(response)
+          const error = response.error as Payjp.ResponseError
+          setTokenGenerationError(error)
+          return
+        }
+        const token = response as Payjp.Token
+        setToken(token.id)
+      })
+      .catch((error: Error) => {
+        console.error(error)
+        setTokenGenerationError(error)
+      })
+    event.preventDefault()
+  }, [payjp, elements])
 
   return (
     <>
-      <div id="number-form" />
-      <div id="expiry-form" />
-      <div id="cvc-form" />
-      <button
-        onClick={event => {
-          payjp.createToken(elements.getElement('cardNumber')).then(r => {
-            setToken(r.error ? r.error.message : r.id)
-          })
-          event.preventDefault()
-        }}
-      >
-        トークンを作成する
-      </button>
-      <div>{token}</div>
+      <Card>
+        <Space direction="vertical">
+          <div id="number-form" />
+          <div id="expiry-form" />
+          <div id="cvc-form" />
+          <Button
+            type="primary"
+            onClick={handleClick}
+          >
+            トークンを作成する
+          </Button>
+          {tokenGenerationError &&
+            <Alert
+              message="エラー"
+              description={tokenGenerationError.message}
+              type="error"
+              showIcon
+            />
+          }
+          {token &&
+            <Alert message={`トークン: ${token}`} type="success" showIcon />
+          }
+        </Space>
+      </Card>
     </>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,28 @@ import { AppProps } from 'next/app'
 import 'antd/dist/antd.css'
 import '@/styles/vars.css'
 import '@/styles/global.css'
+import { Row, Col, Layout, Typography } from 'antd'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Layout>
+        <Layout.Header>
+          <Typography.Text strong style={{color: 'white'}}>
+            PAY.JP + Next.js サンプル
+          </Typography.Text>
+        </Layout.Header>
+        <Layout.Content>
+          <Row>
+            <Col span={2}></Col>
+            <Col span={20}>
+              <Component {...pageProps} />
+            </Col>
+            <Col span={2}></Col>
+          </Row>
+        </Layout.Content>
+        <Layout.Footer>&copy; 2020 Hiroyuki Matsuo</Layout.Footer>
+      </Layout>
+    </>
+  )
 }
